398dc4ea901e465cef20ae1770954f2fb795db28
morita-koki <morita82007koki@gmail.com>
2022-10-15 20:08:07 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom"
 import React from 'react';
 
 const Home = () => {
@@ -26,6 +26,12 @@ function App() {
 
   return (
     <Router basename={ROUTER_BASENAME}>
+      <Link to="/">home</Link>
+      <br></br>
+      <Link to="/calendar">calendar</Link>
+      <br></br>
+      <Link to="/diagram">diagram</Link>
+      <br></br>
       <Routes>
         <Route path="/" exact={true} element={<Home/>}/>
         <Route path="/calendar" element={<Calendar/>}/>
