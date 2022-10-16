d7243d900127d5ed4cc175a2f75f1975dc99760d
morita-koki <morita82007koki@gmail.com>
2022-10-16 15:06:37 +0900

    typescriptを使うように変更＆

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom"
+import React from 'react';
+
+import Home from "./pages/home"
+import Calendar from "./pages/calendar"
+import Diagram from "./pages/diagram"
+
 
 function App() {
+  const ROUTER_BASENAME = "/C_2212"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={ROUTER_BASENAME}>
+      <Link to="/">home</Link>
+      <br></br>
+      <Link to="/calendar">calendar</Link>
+      <br></br>
+      <Link to="/diagram">diagram</Link>
+      <br></br>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/calendar" element={<Calendar/>}/>
+        <Route path="/diagram" element={<Diagram/>}/>
+      </Routes> 
+    </Router>
   );
 }
+
 
 export default App;
