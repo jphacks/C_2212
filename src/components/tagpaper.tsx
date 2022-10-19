7dc89756f203f217c0043e72b81218d505258cd6
kotama7 <kotamatakanori2@gmail.com>
2022-10-19 18:55:00 +0900

    数が数えれるようになった

--- src/components/tagpaper.tsx
+++ src/components/tagpaper.tsx
@@ -1,23 +1,20 @@
 
 import "./tagpaper.css"
 
-        const props =  [
-            {
-                scheduled_date: "10/23(Thu)",
-                task_name: "FocusGold IA"
-            }
-        ]
-        
+
         function choose(props: any) {
-            return ["10/23(Thu) FocusGold IA",""]
+            return ["10/23(Thu) FocusGold IA",]
         }
 
         const TagPaper = (props: any) => {
             let express = choose(props);
             return (
                 <div className="tag_contents" >
-                    <p>{express[0]}</p>
-                    <p>{express[1]}</p>
+                    {express.map((name, index) => {
+                        if(name !== ""){
+                            return(<p>{name}</p>)
+                        }
+                    })}
                 </div>
             )
         }
