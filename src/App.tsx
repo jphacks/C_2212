93dbb55a911dd54930470581ffead68cf3b1eacc
morita-koki <morita82007koki@gmail.com>
2022-10-21 20:29:58 +0900

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 // import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import Home from "./pages/home"
 import Calendar from "./pages/calendar"
-import Diagram from "./pages/diagram"
 // import Tasks from "./pages/tasks"
 import Tasks from "./pages/tasks"
 
