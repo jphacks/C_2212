30af8864dc4af28fd1d08bed7d1553574f7b9846
Tomoki826 <uda.tomoki@icloud.com>
2022-10-22 01:53:26 +0900

    コンフリクト解消

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,11 @@
-// Navbar.js
-import React from "react";
-
+// React Hookをロード
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
+// CSSをロード
 import "./navbar.css";
-// import task_ls from "../lib/data/task_ls.json"
+
+// ローカルファイルをロード
 import { localStorageManager } from "../lib/localstorage/manager";
 
 const NavTaskList = ({tasks}: {tasks: Array<string>}) => {
@@ -22,6 +23,7 @@ const NavTaskList = ({tasks}: {tasks: Array<string>}) => {
 };
 
 const Navbar = () => {
+
   const navigate = useNavigate();
 
   if (!localStorageManager) {
@@ -52,14 +54,13 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <div className="navigation-tasks">
+      <div className="navigation-tasks" style={{cursor: 'pointer'}}>
         <div onClick={() => {navigate("/tasks");}} className="navogation-task-title">Tasks</div>
         <NavTaskList tasks={tasks_name} />
       </div>
     </nav>
   );
 }
-
 
 
 export default Navbar
