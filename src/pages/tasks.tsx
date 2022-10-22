4d004f6c7edd16bfdf140b0e3a653869c8b72cba
morita-koki <morita82007koki@gmail.com>
2022-10-22 12:24:46 +0900

    small fix

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -109,7 +109,7 @@ const Tasks: React.FC = () => {
                             <img src={charteditImg} alt='edit chart' />
                     </div>
                 </div>
-                <TagPapers date={["10/22(Sat)", "10/26(Wed)"]} task_name={["Focus Gold ⅠA", "Focus Gold ⅡB"]} color_number={[0, 1]} />
+                <TagPapers date={["10/22(Sat)", "10/26(Wed)"]} task_name={["Focus Gold IA", "Focus Gold IIB"]} color_number={[0, 1]} />
             </div>
         );
     }
