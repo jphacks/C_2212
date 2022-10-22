f1dcf61e1f420b57fb98a89b1e6b3c704c5fab06
Tomoki826 <uda.tomoki@icloud.com>
2022-10-22 13:40:21 +0900

    ホームページ作成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,14 +5,38 @@ import "./home.css";
 import Navbar from "../components/navbar";
 // import Navbar from "../components/navbar"
 
+import homepageImg from "../images/homepageImage.png";
+
 const Home = () => {
 	
 	return (
-		<>
+		<div className="homepage">
 			<Navbar />
-			<p>this is <b>Home</b> page</p>
-			<p>ホームページ（ミニマムじゃないよ）</p>
-		</>
+			<section className="wrapper2">
+				<div className="container">
+					<div className="content">
+						<h2 className="heading">JunSche</h2>
+						<p>シュンスケ　－順に並べるスケジュール－</p>
+						<p>条件を付けて予定を組めるスケジュール管理アプリ</p>
+					</div>
+				</div>
+			</section>
+			<section className="wrapper">
+			<div className="container">
+				<div className="content">
+					<div className="content-item">
+						<img src={homepageImg} className="image"/>
+					</div>    
+					<div className="content-item">
+						<div className="text">
+							<h2 className="heading">What is JunSche?</h2>
+							<p>多忙な人にとって予定の前後関係の把握にオーバーフローした経験はありませんか？<br/>JunScheを使えば全て解決。カレンダーをフローチャート形式で設計できて、予定の日付はもちろん、今日やるべきこと、次にやるべきこと、予定の条件などを全て"見える化"できます。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			</section>
+		</div>
 	)
 }
 
